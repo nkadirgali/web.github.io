74d4dbf71ea247d69fdde047b6d6a22f3292b59e
nkadirgali <96990699+nkadirgali@users.noreply.github.com>
2022-05-22 09:21:44 +0600

    Create resizeBody.js

---
+++ resizeBody.js
@@ -0,0 +1,4 @@
+window.addEventListener('resize',function (){
+    document.getElementById("body").style.width=document.documentElement.clientWidth+"px";
+//    console.log(document.getElementById("body").style.width+" "+document.documentElement.clientWidth);
+})
