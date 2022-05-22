c369be5003e20de54fbe0298e2f9df73933a01f4
nkadirgali <96990699+nkadirgali@users.noreply.github.com>
2022-05-22 13:06:02 +0600

    Swagger

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs=require('ejs')
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express')
+swaggerDocument = require('./swagger.json');
+const session = require('express-session')
+const passport=require('passport')
+//
+//level 6
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const methodOverride = require('method-override');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
-const UserRoute = require('./routes/User')
-app.use('/user',UserRoute)
+app.use(methodOverride('_method'))
+app.use(session({
+    secret: "then we need to replace it to .env file",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+
 const {router} = require("express/lib/application");
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url, {
@@ -23,15 +40,32 @@ mongoose.connect(dbConfig.url, {
     process.exit();
 });
 
+const UserRoute = require('./routes/User')
+app.use('/user',UserRoute)
+app.use('/contact',require('./routes/Contact'))
+app.use('/post',require('./routes/Post'))
+
 app.use('/',require('./routes/index'));
+app.use('/admin',require('./routes/admin'));
 app.use('/accounts',require('./routes/accounts'));
 app.use('/weather',require('./routes/weather'));
 app.use('/profile',require('./routes/profile'));
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+app.get("/auth/google",
+    passport.authenticate('google',{ scope: ["profile","email"] })
+)
+
+app.get('/auth/google/globolg',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+const port = process.env.PORT || 3000;
+// if (port == null || port == "") {
+//     port = 3000;
+// }
 
 app.listen(port, () => {
     console.log(`Server is listening on port http://localhost:${port}`);
