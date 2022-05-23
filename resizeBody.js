c21b3fc3d4bc625bb633a1fe7dab9e150b8e773d
nkadirgali <96990699+nkadirgali@users.noreply.github.com>
2022-05-23 13:18:18 +0600

    Delete resizeBody.js

--- resizeBody.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('resize',function (){
-    document.getElementById("body").style.width=document.documentElement.clientWidth+"px";
-//    console.log(document.getElementById("body").style.width+" "+document.documentElement.clientWidth);
-})
